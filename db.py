824fbc6e2ec43355c188cca944d653ce296b2e05
gsbharani <gsbharani45@gmail.com>
2026-01-19 16:45:54 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,11 +3,12 @@ import pyodbc
 def get_connection():
     conn = pyodbc.connect(
         'DRIVER={ODBC Driver 18 for SQL Server};'
-        'SERVER=VPRC-SANTHANABH.database.windows.net;;'
+        'SERVER=VPRC-SANTHANABH.database.windows.net;'
         'DATABASE=InventoryDB;'
         'Trusted_Connection=yes;'
         'TrustServerCertificate=yes;'
 
     )
     return conn
+
 
