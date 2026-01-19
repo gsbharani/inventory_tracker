4b55ab24d9c42d900c6b49a90ed28dbcb15b24c8
gsbharani <gsbharani45@gmail.com>
2026-01-19 16:59:55 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,14 +1,47 @@
-import pyodbc
+import sqlite3
 
 def get_connection():
-    conn = pyodbc.connect(
-        'DRIVER={ODBC Driver 18 for SQL Server};'
-        'SERVER=VPRC-SANTHANABH.database.windows.net;'
-        'DATABASE=InventoryDB;'
-        'Trusted_Connection=yes;'
-        'TrustServerCertificate=yes;'
+    return sqlite3.connect("inventory.db", check_same_thread=False)
 
+def create_tables():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS items (
+        item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        item_name TEXT,
+        category TEXT,
+        quantity INTEGER,
+        cost_price REAL,
+        selling_price REAL,
+        min_stock INTEGER
     )
-    return conn
+    """)
 
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS sales (
+        sale_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        item_id INTEGER,
+        qty INTEGER,
+        selling_price REAL,
+        total REAL,
+        customer TEXT,
+        sale_date DATE DEFAULT CURRENT_DATE
+    )
+    """)
 
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS purchases (
+        purchase_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        item_id INTEGER,
+        qty INTEGER,
+        cost_price REAL,
+        total REAL,
+        vendor TEXT,
+        purchase_date DATE DEFAULT CURRENT_DATE
+    )
+    """)
+
+    conn.commit()
+    conn.close()
