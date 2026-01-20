f80f819a851b988c5cfccf0233a7fd9526def40f
gsbharani <gsbharani45@gmail.com>
2026-01-20 10:04:39 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,47 +1,51 @@
 import sqlite3
 
-DB_NAME = "inventory.db"
-
 def get_connection():
-    return sqlite3.connect(DB_NAME, check_same_thread=False)
+    return sqlite3.connect("inventory.db", check_same_thread=False)
 
 def create_tables():
     conn = get_connection()
     cur = conn.cursor()
 
     cur.execute("""
+    CREATE TABLE IF NOT EXISTS vendors (
+        vendor_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        business_name TEXT,
+        email TEXT UNIQUE,
+        password TEXT
+    )
+    """)
+
+    cur.execute("""
     CREATE TABLE IF NOT EXISTS items (
         item_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        item_name TEXT NOT NULL,
-        category TEXT,
-        quantity INTEGER DEFAULT 0,
-        cost_price REAL DEFAULT 0,
-        selling_price REAL DEFAULT 0,
-        min_stock INTEGER DEFAULT 0
+        vendor_id INTEGER,
+        item_name TEXT,
+        quantity INTEGER,
+        cost_price REAL,
+        selling_price REAL
     )
     """)
 
     cur.execute("""
     CREATE TABLE IF NOT EXISTS sales (
         sale_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        vendor_id INTEGER,
         item_id INTEGER,
         qty INTEGER,
-        selling_price REAL,
         total REAL,
-        customer TEXT,
-        sale_date TEXT DEFAULT CURRENT_DATE
+        date TEXT DEFAULT CURRENT_DATE
     )
     """)
 
     cur.execute("""
     CREATE TABLE IF NOT EXISTS purchases (
         purchase_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        vendor_id INTEGER,
         item_id INTEGER,
         qty INTEGER,
-        cost_price REAL,
-        total REAL,
-        vendor TEXT,
-        purchase_date TEXT DEFAULT CURRENT_DATE
+        total_cost REAL,
+        date TEXT DEFAULT CURRENT_DATE
     )
     """)
 
