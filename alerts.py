0485bb7646635256e868ef8afc2063713644007b
gsbharani <gsbharani45@gmail.com>
2026-01-23 12:32:58 +0530

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -1,1 +1,10 @@
+from twilio.rest import Client
+
+def send_whatsapp(msg):
+    client = Client("SID", "TOKEN")
+    client.messages.create(
+        from_="whatsapp:+14155238886",
+        to="whatsapp:+91XXXXXXXXXX",
+        body=msg
+    )
 
