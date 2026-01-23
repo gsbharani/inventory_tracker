dfe0428e9910d9d94fcfd4c2941e749af7e5be28
gsbharani <gsbharani45@gmail.com>
2026-01-23 16:09:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
-from db import create_tables
 from auth import login, signup
+from dashboard import dashboard_page
 from items import items_page
 from sales import sales_page
 from profit import profit_page
 
 st.set_page_config("Inventory SaaS", layout="wide")
-create_tables()
 
 if "vendor_id" not in st.session_state:
     choice = st.radio("Choose", ["Login", "Sign Up"])
@@ -16,9 +15,14 @@ if "vendor_id" not in st.session_state:
         signup()
 else:
     st.sidebar.success(f"Welcome {st.session_state.vendor_name}")
-    page = st.sidebar.selectbox("Menu", ["Items", "Sales", "Profit"])
 
-    if page == "Items":
+    page = st.sidebar.selectbox(
+        "Menu", ["Dashboard", "Items", "Sales", "Profit"]
+    )
+
+    if page == "Dashboard":
+        dashboard_page()
+    elif page == "Items":
         items_page()
     elif page == "Sales":
         sales_page()
