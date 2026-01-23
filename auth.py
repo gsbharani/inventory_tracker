678bd2dcb0a91b6ebb07b4677341da6aae8a9336
gsbharani <gsbharani45@gmail.com>
2026-01-23 12:31:01 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,11 @@
+from db import get_connection
+
+def login(email, password):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT vendor_id, name FROM vendors WHERE email=%s AND password=%s",
+        (email, password)
+    )
+    return cur.fetchone()
 
