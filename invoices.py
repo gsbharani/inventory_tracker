82383c6309adfc2ead43e77f2476309205a2f453
gsbharani <gsbharani45@gmail.com>
2026-01-23 12:32:21 +0530

    Update invoices.py

--- invoices.py
+++ invoices.py
@@ -1,21 +1,7 @@
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 
-def generate_invoice(invoice_no, customer, item, qty, price):
-    filename = f"invoice_{invoice_no}.pdf"
+def generate_invoice(filename):
     c = canvas.Canvas(filename, pagesize=A4)
-
-    c.setFont("Helvetica-Bold", 14)
-    c.drawString(50, 800, "INVOICE")
-
-    c.setFont("Helvetica", 10)
-    c.drawString(50, 770, f"Invoice No: {invoice_no}")
-    c.drawString(50, 750, f"Customer: {customer}")
-
-    c.drawString(50, 700, f"Item: {item}")
-    c.drawString(50, 680, f"Quantity: {qty}")
-    c.drawString(50, 660, f"Price: ₹{price}")
-    c.drawString(50, 640, f"Total: ₹{qty * price}")
-
+    c.drawString(100, 800, "INVOICE")
     c.save()
-    return filename
