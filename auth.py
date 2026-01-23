f5d2d2d137f998cbd7f2901c0222fc88fa47f689
gsbharani <gsbharani45@gmail.com>
2026-01-23 13:54:09 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,54 +1,30 @@
 import streamlit as st
 from db import get_connection
 
-def signup():
-    st.subheader("Create Vendor Account")
-
-    business = st.text_input("Business Name")
-    email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
-
-    if st.button("Sign Up"):
-        conn = get_connection()
-        cur = conn.cursor()
-
-        try:
-            cur.execute(
-                """
-                INSERT INTO vendors (business_name, email, password)
-                VALUES (%s, %s, %s)
-                """,
-                (business, email, password)
-            )
-            conn.commit()
-            st.success("Account created. Please login.")
-        except Exception as e:
-            st.error("Email already exists")
-
-def login():
-    st.subheader("Vendor Login")
-
-    email = st.text_input("Email")
-    password = st.text_input("Password", type="password")
-
-    if st.button("Login"):
-        conn = get_connection()
-        cur = conn.cursor()
-
+def signup(name, email, password):
+    conn = get_connection()
+    cur = conn.cursor()
+    try:
         cur.execute(
-            """
-            SELECT vendor_id, business_name
-            FROM vendors
-            WHERE email=%s AND password=%s
-            """,
-            (email, password)
+            "INSERT INTO vendors (name, email, password) VALUES (%s, %s, %s) RETURNING vendor_id;",
+            (name, email, password)
         )
+        vendor_id = cur.fetchone()[0]
+        conn.commit()
+        return vendor_id
+    except Exception as e:
+        st.error("Email already exists")
+        return None
+    finally:
+        conn.close()
 
-        vendor = cur.fetchone()
-
-        if vendor:
-            st.session_state["vendor_id"] = vendor[0]
-            st.session_state["vendor_name"] = vendor[1]
-            st.rerun()
-        else:
-            st.error("Invalid login")
+def login(email, password):
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT vendor_id, name FROM vendors WHERE email=%s AND password=%s;",
+        (email, password)
+    )
+    user = cur.fetchone()
+    conn.close()
+    return user
